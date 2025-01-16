e18711082feacd58069a2aaebbe1796d6a576d7e
Sony Setiawan <sonysetiawanred@gmail.com>
2025-01-16 16:22:00 +0700

    feat: i18n

---
+++ src/utils/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpBackend from 'i18next-http-backend';
+
+i18n
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: import.meta.env.VITE_MODE === 'development',
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    ns: ['common', 'i18n'],
+    defaultNS: 'common',
+  });
+
+export { i18n };
