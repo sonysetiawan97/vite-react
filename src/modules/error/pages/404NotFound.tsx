a04584e38566ab641844e3c8eb361fc8fbff51d5
Sony Setiawan <sonysetiawanred@gmail.com>
2025-03-19 15:27:20 +0700

    fix: 404 error page translate

--- src/modules/error/pages/404NotFound.tsx
+++ src/modules/error/pages/404NotFound.tsx
@@ -1,17 +1,20 @@
-import type { FC } from 'react';
+import type { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 const NotFound: FC = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="page404">
       <div className="d-flex flex-column justify-content-between align-items-center h-100 py-5">
-        <div className='text-center'>
-          <h1 className='display-2 mb-0'>Oops!</h1>
-          <h5 className='text-muted'>Page Not Found</h5>
+        <div className="text-center">
+          <h1 className="display-2 mb-0">Oops!</h1>
+          <h5 className="text-muted">Page Not Found</h5>
         </div>
-          <div className="img"></div>
+        <div className="img" />
 
-        <a href="/dashboard">Back</a>
-
+        <Link to={"/dashboard"}>{t("error.404_not_found.button")}</Link>
       </div>
     </div>
   );
