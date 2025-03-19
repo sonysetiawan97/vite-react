4fd6e5b2f32d2be51fb73ee5975c89db76936c4c
adoel razman <adoel4razman@gmail.com>
2025-03-19 10:24:35 +0700

    styling page 404

--- src/modules/error/pages/404NotFound.tsx
+++ src/modules/error/pages/404NotFound.tsx
@@ -2,9 +2,17 @@ import type { FC } from 'react';
 
 const NotFound: FC = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="page404">
+      <div className="d-flex flex-column justify-content-between align-items-center h-100 py-5">
+        <div className='text-center'>
+          <h1 className='display-2 mb-0'>Oops!</h1>
+          <h5 className='text-muted'>Page Not Found</h5>
+        </div>
+          <div className="img"></div>
+
+        <a href="/dashboard">Back</a>
+
+      </div>
     </div>
   );
 };
